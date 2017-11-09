40a493022666a4a7851aa00e6a704df3a8498516
Jeremy Mundell <jdmundell@vectorsecurity.com>
2017-11-09 13:33:27 -0500

    add new items
    added new items

---
+++ app.js
@@ -0,0 +1,12 @@
+// alert("connected");
+
+// document.write("hello world!!!");
+
+var cars = [ "lambo ", "civic ", "porsche ", "van ", "tesla "];
+
+
+for (i = 0; i < cars.length; i ++){
+	document.write(cars[i]);
+};
+
+	
